4403c435f56378d67b2226eb81ed5bb65f8e10cb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-05-09 15:33:01 +0300

    push me

---
+++ pages/api/send-email.js
@@ -0,0 +1,14 @@
+import { sendEmail } from '../../utils/sendEmail';
+
+export default async (req, res) => {
+  try {
+    if (req.method === 'POST') {
+      const { body } = req;
+      console.log(body);
+      await sendEmail(body);
+      return res.status(200).end();
+    }
+  } catch (error) {
+    return res.json({ error });
+  }
+};
